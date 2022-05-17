3723edb4b942be00771f1a8758829e12775447c7
Bamidele Popoola <32906210+bamibanzai@users.noreply.github.com>
2022-05-17 16:47:52 +0100

    change 4 commit

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 # Navigation Bar Added
-# CHANGE 1
-# CHANGE 2
-# CHANGE 3
-# CHANGE 4
+# DEVELOPER 2 Change 1
 print("Hi I've added the navigation Bar")
+
+
+
+# Dev 2 change 2
+
+
+# Dev 2 change 3
+
+
+# Dev 2 change 4
